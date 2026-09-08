2d7e12ba49935c87ed452bc276f5ad2d0eaf99b9
Mathieu Deliens <127595205+MDELS24@users.noreply.github.com>
2026-09-08 19:25:41 +0200

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// Cette clé publiable est utilisable dans le navigateur avec RLS activé.
-window.SUPABASE_CONFIG = {
-  url: 'https://cuuihwxcohhhnpbizsyv.supabase.co',
-  publishableKey: 'sb_publishable_40GsUzg7Vi-CzMm62peyyw_7FHxsApV'
-};
